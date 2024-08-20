c40d8080f735667794106bccdd6a20d96e95c089
MINEDUCYT <MINEDUCYT@LAPTOP-98BOIG05>
2024-08-19 19:27:24 -1200

    Agregar archivos de proyecto.

---
+++ CrudNet4700154/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CrudNet4700154
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
